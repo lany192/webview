92205b55665aba7e68e6dcd14bbfadb5355ca0a3
Lany <lany192@qq.com>
2017-07-11 11:50:49 +0800

    upgrade build version

--- library/src/main/java/com/lany/library/ProgressWebView.java
+++ library/src/main/java/com/lany/library/ProgressWebView.java
@@ -15,14 +15,10 @@ public class ProgressWebView extends WebView {
 
 	public ProgressWebView(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		mProgressBar = new ProgressBar(context, null,
-				android.R.attr.progressBarStyleHorizontal);
-		LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
-				LinearLayout.LayoutParams.MATCH_PARENT, 8);
+		mProgressBar = new ProgressBar(context, null, android.R.attr.progressBarStyleHorizontal);
+		LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 8);
 		mProgressBar.setLayoutParams(layoutParams);
-
-		Drawable drawable = context.getResources().getDrawable(
-				R.drawable.web_progress_bar_states);
+		Drawable drawable = context.getResources().getDrawable(R.drawable.web_progress_bar_states);
 		mProgressBar.setProgressDrawable(drawable);
 		addView(mProgressBar);
 		setWebChromeClient(new WebChromeClient());
