006f17c0be223a9507794710cb77bd90f840b631
lany192 <lany192@qq.com>
2021-12-03 11:33:43 +0800

    update

--- webkit/src/main/java/com/github/lany192/webkit/ProgressWebView.java
+++ webkit/src/main/java/com/github/lany192/webkit/ProgressWebView.java
@@ -3,7 +3,9 @@ package com.github.lany192.webkit;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 
@@ -22,6 +24,33 @@ public class ProgressWebView extends WebView {
 		mProgressBar.setProgressDrawable(drawable);
 		addView(mProgressBar);
 		setWebChromeClient(new WebChromeClient());
+		this.setWebViewClient(new WebViewClient() {
+			//防止加载网页时调起系统浏览器
+			public boolean shouldOverrideUrlLoading(WebView view, String url) {
+				view.loadUrl(url);
+				return true;
+			}
+		});
+		WebSettings webSetting = this.getSettings();
+		webSetting.setJavaScriptEnabled(true);
+		webSetting.setJavaScriptCanOpenWindowsAutomatically(true);
+		webSetting.setAllowFileAccess(true);
+		webSetting.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+		webSetting.setSupportZoom(true);
+		webSetting.setBuiltInZoomControls(true);
+		webSetting.setUseWideViewPort(true);
+		webSetting.setSupportMultipleWindows(true);
+		// webSetting.setLoadWithOverviewMode(true);
+		webSetting.setAppCacheEnabled(true);
+		// webSetting.setDatabaseEnabled(true);
+		webSetting.setDomStorageEnabled(true);
+		webSetting.setGeolocationEnabled(true);
+		webSetting.setAppCacheMaxSize(Long.MAX_VALUE);
+		// webSetting.setPageCacheCapacity(IX5WebSettings.DEFAULT_CACHE_CAPACITY);
+		webSetting.setPluginState(WebSettings.PluginState.ON_DEMAND);
+		// webSetting.setRenderPriority(WebSettings.RenderPriority.HIGH);
+		webSetting.setCacheMode(WebSettings.LOAD_NO_CACHE);
+		setClickable(true);
 	}
 
 	public class WebChromeClient extends android.webkit.WebChromeClient {
