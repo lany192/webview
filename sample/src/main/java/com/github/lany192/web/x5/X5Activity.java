2ed8b0fcffdad197e5212f7e62846158f259df60
lany192 <lany192@qq.com>
2021-12-03 14:05:12 +0800

    update

--- sample/src/main/java/com/github/lany192/web/x5/X5Activity.java
+++ sample/src/main/java/com/github/lany192/web/x5/X5Activity.java
@@ -2,46 +2,22 @@ package com.github.lany192.web.x5;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.lany192.web.R;
 import com.github.lany192.x5web.X5WebView;
-import com.tencent.smtt.sdk.WebChromeClient;
-import com.tencent.smtt.sdk.WebView;
-import com.tencent.smtt.sdk.WebViewClient;
 
 public class X5Activity extends AppCompatActivity {
-    X5WebView mWebView;
-    ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_x5);
-        mWebView = (X5WebView) findViewById(R.id.web_view);
-        mProgressBar = (ProgressBar) findViewById(R.id.web_progressbar);
+        X5WebView webView = (X5WebView) findViewById(R.id.web_view);
 
         Intent intent = getIntent();
         String url = intent.getStringExtra("url");
-
-        mWebView.setWebChromeClient(new MyWebChromeClient());
-        mWebView.loadUrl(url);
-    }
-
-    public class MyWebChromeClient extends WebChromeClient {
-        @Override
-        public void onProgressChanged(WebView view, int newProgress) {
-            if (newProgress == 100) {
-                mProgressBar.setVisibility(View.GONE);
-            } else {
-                if (mProgressBar.getVisibility() == View.GONE)
-                    mProgressBar.setVisibility(View.VISIBLE);
-                mProgressBar.setProgress(newProgress);
-            }
-            super.onProgressChanged(view, newProgress);
-        }
+        webView.loadUrl(url);
     }
 }
