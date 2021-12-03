35c5721a6751f1464a4eda48c0512624abb60b7a
lany192 <lany192@qq.com>
2021-12-03 11:37:59 +0800

    update

--- sample/src/main/java/com/github/lany192/web/MainActivity.java
+++ sample/src/main/java/com/github/lany192/web/MainActivity.java
@@ -2,16 +2,16 @@ package com.github.lany192.web;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.lany192.web.webkit.Web1Activity;
 import com.github.lany192.web.webkit.Web2Activity;
+import com.github.lany192.web.x5.X5Activity;
 
 public class MainActivity extends AppCompatActivity {
-    EditText mEditText;
+    private EditText mEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,25 +19,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mEditText = (EditText) findViewById(R.id.url_input);
         mEditText.setText("https://www.qq.com/");
-        findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                String url = mEditText.getText().toString().trim();
-                Intent intent = new Intent(MainActivity.this, Web1Activity.class);
-                intent.putExtra("url", url);
-                startActivity(intent);
-            }
+        findViewById(R.id.button1).setOnClickListener(v -> {
+            String url = mEditText.getText().toString().trim();
+            Intent intent = new Intent(this, Web1Activity.class);
+            intent.putExtra("url", url);
+            startActivity(intent);
         });
-        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                String url = mEditText.getText().toString().trim();
-                Intent intent = new Intent(MainActivity.this, Web2Activity.class);
-                intent.putExtra("url", url);
-                startActivity(intent);
-            }
+        findViewById(R.id.button2).setOnClickListener(v -> {
+            String url = mEditText.getText().toString().trim();
+            Intent intent = new Intent(this, Web2Activity.class);
+            intent.putExtra("url", url);
+            startActivity(intent);
+        });
+        findViewById(R.id.button3).setOnClickListener(v -> {
+            String url = mEditText.getText().toString().trim();
+            Intent intent = new Intent(this, X5Activity.class);
+            intent.putExtra("url", url);
+            startActivity(intent);
         });
     }
 }
--- sample/src/main/java/com/github/lany192/web/x5/X5Activity.java
+++ sample/src/main/java/com/github/lany192/web/x5/X5Activity.java
@@ -1,16 +1,56 @@
 package com.github.lany192.web.x5;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.github.lany192.web.R;
+import com.github.lany192.x5web.X5WebView;
+import com.tencent.smtt.sdk.WebChromeClient;
+import com.tencent.smtt.sdk.WebView;
+import com.tencent.smtt.sdk.WebViewClient;
 
 public class X5Activity extends AppCompatActivity {
+    X5WebView mWebView;
+    ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_x5);
+        mWebView = (X5WebView) findViewById(R.id.web_view);
+        mProgressBar = (ProgressBar) findViewById(R.id.web_progressbar);
+
+        Intent intent = getIntent();
+        String url = intent.getStringExtra("url");
+
+        mWebView.setWebChromeClient(new MyWebChromeClient());
+        mWebView.setWebViewClient(webViewClient);
+        mWebView.loadUrl(url);
+    }
+
+    private WebViewClient webViewClient = new WebViewClient() {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    };
+
+    public class MyWebChromeClient extends WebChromeClient {
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            if (newProgress == 100) {
+                mProgressBar.setVisibility(View.GONE);
+            } else {
+                if (mProgressBar.getVisibility() == View.GONE)
+                    mProgressBar.setVisibility(View.VISIBLE);
+                mProgressBar.setProgress(newProgress);
+            }
+            super.onProgressChanged(view, newProgress);
+        }
     }
 }
